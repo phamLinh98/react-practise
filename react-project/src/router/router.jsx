1c685a50b7a24b460ce547fa1e34b2157f3aa8ad
“linhPham98” <linhthusinh98@gmail>
2024-06-23 14:27:28 +0900

    delete router grammar and japanese

--- react-project/src/router/router.jsx
+++ react-project/src/router/router.jsx
@@ -1,30 +1,14 @@
 import DemoComponent from "../FeatureComponent/Demo";
 import Game from "../FeatureComponent/Game";
-import JapaneseFeature, {
-  CheckGrammaJapanese,
-} from "../FeatureComponent/JapaneseTranslate";
-import Translate from "../FeatureComponent/Translate";
 
 export const rootConfig = [
   {
     path: "/",
-    element: <JapaneseFeature />,
-  },
-  {
-    path: "/japanese",
-    element: <JapaneseFeature />,
+    element: <Game/>
   },
   {
     path: "/demo",
     element: <DemoComponent />,
-  },
-  {
-    path: "/grammar",
-    element: <CheckGrammaJapanese />,
-  },
-  {
-    path: "/translate",
-    element: <Translate />,
   },
   {
     path: "/game",
